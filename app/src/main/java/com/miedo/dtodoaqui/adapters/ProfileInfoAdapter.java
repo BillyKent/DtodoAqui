ee81b60879c6d89f52e16953dd4a87dc0aa527d9
BillyKentAC <billykentac@gmail.com>
2019-06-08 11:41:31 -0500

    avance profile screen

---
+++ app/src/main/java/com/miedo/dtodoaqui/adapters/ProfileInfoAdapter.java
@@ -0,0 +1,111 @@
+package com.miedo.dtodoaqui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.miedo.dtodoaqui.R;
+
+import java.util.ArrayList;
+
+public class ProfileInfoAdapter extends ArrayAdapter<ProfileInfoAdapter.ProfileItem> {
+
+    private final Context mContext;
+    private final ArrayList<ProfileItem> mData;
+
+    public ProfileInfoAdapter(Context mContext, ArrayList<ProfileItem> mData) {
+        super(mContext, R.layout.cardview_profile_info, mData);
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    private int lastPosition = -1;
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ProfileItem item = getItem(position);
+
+        ViewHolder viewHolder;
+
+        final View result;
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(R.layout.cardview_profile_info, parent, false);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.infoTypeTextView);
+            viewHolder.icon = (ImageView) convertView.findViewById(R.id.infoTypeImageView);
+            viewHolder.content = (TextView) convertView.findViewById(R.id.contentInfoTextView);
+
+            result = convertView;
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+
+        Animation animation = AnimationUtils.loadAnimation(mContext, (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
+        result.startAnimation(animation);
+        lastPosition = position;
+
+        viewHolder.title.setText(item.getTitle());
+        viewHolder.content.setText(item.getContent());
+        viewHolder.icon.setImageResource(item.getResIcon());
+        viewHolder.icon.setTag(position);
+
+        return convertView;
+    }
+
+    private static final class ViewHolder {
+        ImageView icon;
+        TextView title;
+        TextView content;
+
+    }
+
+    public static class ProfileItem {
+        private int resIcon;
+        private String title;
+        private String content;
+
+        public ProfileItem(int resIcon, String title, String content) {
+            this.resIcon = resIcon;
+            this.title = title;
+            this.content = content;
+        }
+
+        public int getResIcon() {
+            return resIcon;
+        }
+
+        public void setResIcon(int resIcon) {
+            this.resIcon = resIcon;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+    }
+}
--- app/src/main/java/com/miedo/dtodoaqui/presentation/fragments/LoggedFragment.java
+++ app/src/main/java/com/miedo/dtodoaqui/presentation/fragments/LoggedFragment.java
@@ -7,13 +7,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.miedo.dtodoaqui.R;
+import com.miedo.dtodoaqui.adapters.ProfileInfoAdapter;
 import com.miedo.dtodoaqui.core.BaseFragment;
+
+import java.util.ArrayList;
 
 
 public class LoggedFragment extends BaseFragment {
 
+    ArrayList<ProfileInfoAdapter.ProfileItem> items;
+    ListView listView;
+    private ProfileInfoAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +32,26 @@ public class LoggedFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_logged_profile, container, false);
 
         return view;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        listView = (ListView) view.findViewById(R.id.list_details);
+        items = new ArrayList<>();
+        loadItems();
+        adapter = new ProfileInfoAdapter(getContext(),items);
+        listView.setAdapter(adapter);
+    }
+
+    void loadItems() {
+
+        items.add(new ProfileInfoAdapter.ProfileItem(R.drawable.ic_account_box_black_24dp, "Nombre", "Sopa do macaco"));
+        items.add(new ProfileInfoAdapter.ProfileItem(R.drawable.ic_phone_black_24dp, "Telefono", "+51994224498"));
+        items.add(new ProfileInfoAdapter.ProfileItem(R.drawable.ic_location_on_black_24dp, "Dirección", "Av.Brasil #322"));
+        items.add(new ProfileInfoAdapter.ProfileItem(R.drawable.ic_facebook, "Nombre", "https://www.facebook.com/sopademacacodelicia/"));
+
     }
 
 }
