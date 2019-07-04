e98e411e1170217fc520dffeab65f2d207f33c92
Luis Huayta <billykentac@gmail.com>
2019-07-04 17:37:52 -0500

    reviews xd

---
+++ app/src/main/java/com/miedo/dtodoaqui/presentation/activities/PostReviewActivity.java
@@ -0,0 +1,19 @@
+package com.miedo.dtodoaqui.presentation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.miedo.dtodoaqui.R;
+
+public class PostReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_review);
+
+
+
+    }
+}
